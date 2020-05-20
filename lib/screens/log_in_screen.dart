b1da19d54f49cff04d83fe2763add29c1e7532cf
byronwezvo <byronwezvo@outlook.com>
2020-05-20 10:35:08 +0200

    created log in screen

---
+++ lib/screens/log_in_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LogInScreen extends StatefulWidget {
+  @override
+  _LogInScreenState createState() => _LogInScreenState();
+}
+
+class _LogInScreenState extends State<LogInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

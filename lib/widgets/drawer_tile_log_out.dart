93e8a8518b285bd59087e58e00b16d8c022863bc
byronwezvo <byronwezvo@outlook.com>
2020-05-28 19:35:19 +0200

    Added Splash to Logout tile => Left side drawer

--- lib/widgets/drawer_tile_log_out.dart
+++ lib/widgets/drawer_tile_log_out.dart
@@ -15,43 +15,43 @@ class DrawerTileLogOut extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return GestureDetector(
-      onTap: () {
-        // TODO : Call api for user log out
-        Navigator.pop(context, main());
-        Navigator.pushNamed(context, 'login');
-      },
-      child: Container(
-        height: 60,
-        decoration: BoxDecoration(
-          color: appColorBlack,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              width: size.width / 8,
-              child: Center(
-                child: Dot(
-                  color: appColorMaroon,
-                ),
-              ),
-            ),
-            Container(
-              width: (size.width / 2) + 20,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 5.0),
-                child: Text(
-                  name,
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: appColorPeach,
-                    fontFamily: 'Nova',
+    return Material(
+      color: appColorBlack,
+      child: InkWell(
+        onTap: () {
+          // TODO : Call api for user log out
+          Navigator.pop(context, main());
+          Navigator.pushReplacementNamed(context, 'login');
+        },
+        child: Container(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: size.width / 8,
+                child: Center(
+                  child: Dot(
+                    color: appColorMaroon,
                   ),
                 ),
               ),
-            ),
-          ],
+              Container(
+                width: (size.width / 2) + 20,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 5.0),
+                  child: Text(
+                    name,
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: appColorPeach,
+                      fontFamily: 'Nova',
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
